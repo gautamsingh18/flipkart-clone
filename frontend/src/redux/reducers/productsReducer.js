f0b24b65793b4e6845945c1efd6b5417857559a2
gautamSynamedia <gasingh@synamedia.com>
2024-06-06 23:07:47 +0530

    fixed wrong constant import

--- frontend/src/redux/reducers/productsReducer.js
+++ frontend/src/redux/reducers/productsReducer.js
@@ -4,7 +4,7 @@ export const getProductsReducer = (state = { products: [] }, action) => {
   switch (action.type) {
     case actionTypes.GET_PRODUCTS_SUCCESS:
       return { products: action.payload };
-    case actionTypes.GET_PRODUCTS_FAIL:
+    case actionTypes.GET_PRODUCTS_FAILURE:
       return { error: action.payload };
     default:
       return state;
@@ -17,7 +17,7 @@ export const getProductDetailsReducer = (state = { product: {} }, action) => {
       return { loading: true };
     case actionTypes.GET_PRODUCTS_DETAILS_SUCCESS:
       return { product: action.payload, loading: false };
-    case actionTypes.GET_PRODUCTS_DETAILS_FAIL:
+    case actionTypes.GET_PRODUCTS_DETAILS_FAILURE:
       return { error: action.payload, loading: false };
     case actionTypes.GET_PRODUCTS_RESET:
       return { product: {}, loading: false };
