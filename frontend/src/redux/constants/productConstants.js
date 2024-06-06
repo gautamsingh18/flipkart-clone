642af8858c917868a8f7337bcfe7488ab60afa80
gautamsingh18 <65909262+gautamsingh18@users.noreply.github.com>
2024-06-06 23:17:09 +0530

    Update productConstants.js

--- frontend/src/redux/constants/productConstants.js
+++ frontend/src/redux/constants/productConstants.js
@@ -2,5 +2,5 @@ export const GET_PRODUCTS_SUCCESS = "getProductsSuccess";
 export const GET_PRODUCTS_FAILURE = "getProductsFailure";
 export const GET_PRODUCTS_DETAILS_REQUEST = "getProductDetailsRequest";
 export const GET_PRODUCTS_DETAILS_SUCCESS = "getProductDetailsSuccess";
-export const GET_PRODUCTS_DETAILS_FAIL = "getProductDetailsFail";
+export const GET_PRODUCTS_DETAILS_FAILURE = "getProductDetailsFail";
 export const GET_PRODUCTS_RESET = "getProductDetailsReset";
